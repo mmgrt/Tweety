23c506c2a5713a13106830872d553abe035906c5
Muhamed ALGHZAWI <m.mg@outlook.com>
2017-07-21 17:42:55 +0300

    Adding tests project

---
+++ Tweety.Tests/TestsConstants.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweety.Authentication;
+
+namespace Tweety.Tests
+{
+    public class TestsConstants
+    {
+        /// <summary>
+        /// Auth context to use int tests.
+        /// </summary>
+        public static TweetyAuthContext AuthContext => new TweetyAuthContext()
+        {
+            AccessSecret = "",
+            AccessToken = "",
+            ConsumerKey = "",
+            ConsumerSecret = ""
+        };
+
+
+        /// <summary>
+        /// Webhook Url to register.
+        /// </summary>
+        public static string WebhookUrl => "";
+
+
+        /// <summary>
+        /// Webhook Id to test against, Will be set in the RegisterWebhookTest. 
+        /// </summary>
+        public static string RegsiteredWebhookId { get; set; } = "";
+    }
+}
---
+++ Tweety.Tests/WebhookManagerTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Tweety.Authentication;
+using static Tweety.Tests.TestsConstants;
+using Tweety.Webhooks;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tweety.Tests
+{
+    [TestClass]
+    public class WebhookManagerTests
+    {
+        [TestMethod]
+        [Priority(0)]
+        public async Task GetRegisteredWebhookTest()
+        {
+
+            WebhooksManager webhookManager = new WebhooksManager(AuthContext);
+
+            var result = await webhookManager.GetRegisteredWebhooks();
+
+            Assert.IsTrue(result.Success);
+            Assert.IsTrue(result.Data.Count == 0);
+
+        }
+
+        [TestMethod]
+        [Priority(1)]
+        public async Task RegisterWebhookTest()
+        {
+            WebhooksManager webhookManager = new WebhooksManager(AuthContext);
+
+            var result = await webhookManager.RegisterWebhook(WebhookUrl);
+
+            Assert.IsTrue(result.Success);
+            Assert.IsTrue(result.Data.IsValid);
+            Assert.IsTrue(result.Data.RegisteredUrl == WebhookUrl);
+
+            RegsiteredWebhookId = result.Data.Id;
+        }
+
+
+
+        [TestMethod]
+        [Priority(2)]
+        public async Task UnregisterWebhookTest()
+        {
+            WebhooksManager webhookManager = new WebhooksManager(AuthContext);
+
+            var result = await webhookManager.UnregisterWebhook(RegsiteredWebhookId);
+
+            Assert.IsTrue(result.Success);
+            Assert.IsTrue(result.Data);
+
+        }
+
+    }
+}
